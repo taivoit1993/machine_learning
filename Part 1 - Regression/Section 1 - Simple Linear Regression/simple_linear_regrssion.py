318c7896344fac00bcfb64384ace1565d96f4f57
taivoit1993 <48310734+taivoit1993@users.noreply.github.com>
2020-02-15 20:21:35 +0700

    Update Simple Linear Regression

--- Part 1 - Regression/Section 1 - Simple Linear Regression/simple_linear_regrssion.py
+++ Part 1 - Regression/Section 1 - Simple Linear Regression/simple_linear_regrssion.py
@@ -10,25 +10,6 @@ dataset = pd.read_csv('Salary_Data.csv');
 X = dataset.iloc[:,0].values
 y = dataset.iloc[:,-1].values
 
-#processing missing data
-'''from sklearn.impute import SimpleImputer
-imp_mean = SimpleImputer(strategy='mean')
-imp_mean.fit(X[:,1:3])
-X[:,1:3] = imp_mean.transform(X[:,1:3])'''
-
-#processing label encode
-#from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-#label encoder
-'''labelencoder_X = LabelEncoder()
-X[:,0] = labelencoder_X.fit_transform(X[:,0])'''
-
-#Onhot Encoder
-'''onehotencoder = OneHotEncoder(categorical_features=[0])
-X = onehotencoder.fit_transform(X).toarray()
-
-labelencoder_y = LabelEncoder()
-y = labelencoder_y.fit_transform(y)'''
-
 # Splitting the dataset into the Trainning set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2,random_state=0)
@@ -42,6 +23,18 @@ print(regressor.coef_)
 #predicting the Test set results
 y_pred = regressor.predict(X_test.reshape(-1,1)).flatten()
 
-#visualising the test set result
+#visualising the train and test result
+plt.scatter(X_train, y_train, c='r');
+plt.plot(X_train, regressor.predict(X_train.reshape(-1,1)).flatten(),c='b')
+plt.title('Salary and Experience (Train)')
+plt.xlabel('Years')
+plt.ylabel('Salary')
+plt.show();
 
+plt.scatter(X_test, y_test, c='r');
+plt.plot(X_train, regressor.predict(X_train.reshape(-1,1)).flatten(),c='b')
+plt.title('Salary and Experience (Test)')
+plt.xlabel('Years')
+plt.ylabel('Salary')
+plt.show();
 
