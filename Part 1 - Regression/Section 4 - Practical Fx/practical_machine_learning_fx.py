612f0c0dd1bf7f464a2be9df37595f4d83506285
taivoit1993 <48310734+taivoit1993@users.noreply.github.com>
2020-02-18 23:30:32 +0700

    Update Precditing Price Eur/Usd depend Usd Index

--- Part 1 - Regression/Section 4 - Practical Fx/practical_machine_learning_fx.py
+++ Part 1 - Regression/Section 4 - Practical Fx/practical_machine_learning_fx.py
@@ -21,4 +21,32 @@ X = dataset.iloc[:,-1].values
 #processing missing data
 from sklearn.impute import SimpleImputer
 imp_mean = SimpleImputer(strategy="mean")
-X = imp_mean.fit_transform(X.reshape(-1,1)).flater()
+X = imp_mean.fit_transform(X.reshape(-1,1)).flatten()
+
+#plitting dataset into trainning set and test set
+y = y.flatten()
+from sklearn.model_selection import train_test_split
+X_train, X_test , y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=0)
+
+#processing Simple Linear Regression
+from sklearn.linear_model import LinearRegression
+linear_regression = LinearRegression()
+linear_regression.fit(X_train.reshape(-1,1),y_train.reshape(-1,1))
+
+#predicting
+y_predicting = linear_regression.predict(X_test.reshape(-1,1)).flatten()
+
+#visualizing 
+plt.scatter(X_train, y_train, c='r')
+plt.plot(X_train,linear_regression.predict(X_train.reshape(-1,1)).flatten(),c="b")
+plt.title('USD Index AND USD/EUR')
+plt.xlabel('USD INDEX')
+plt.ylabel('EUR/USD')
+plt.show()
+
+plt.scatter(X_test, y_test, c='r')
+plt.plot(X_train,linear_regression.predict(X_train.reshape(-1,1)).flatten(),c="b")
+plt.title('USD Index AND USD/EUR')
+plt.xlabel('USD INDEX')
+plt.ylabel('EUR/USD')
+plt.show()
